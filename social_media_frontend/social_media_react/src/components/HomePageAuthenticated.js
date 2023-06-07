9d980eaa41668ee0635af966d34abcd40a62c798
tanujsonare <tanujsonare1910@gmail.com>
2023-06-07 12:55:37 +0530

    Updated UI for Home page

--- social_media_frontend/social_media_react/src/components/HomePageAuthenticated.js
+++ social_media_frontend/social_media_react/src/components/HomePageAuthenticated.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
 import NavbarForAuthenticated from './NavbarForAuthenticated'
+import backgroundImage from './images/social_media_back.jpg'
 
 export default function HomePageAuthenticated(props) {
   return (
-    <>
+    <div style={{backgroundImage:`url(${backgroundImage})`,backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', width: '100%', height: '600px'}}>
         <NavbarForAuthenticated userName={props.userName} userToken={props.userToken}/>
-        <div>
+        <div className='my-4 text-light'>
             You are loggend In.
         </div>
-    </>
+    </div>
   )
 }
