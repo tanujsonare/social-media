81a9e35563fbcff7c5b2da1d8cffe2856dfcc3c7
tanujsonare <tanujsonare1910@gmail.com>
2023-06-07 19:09:51 +0530

    created new api to get tweet

--- social_media_backend/social_media/urls.py
+++ social_media_backend/social_media/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/get_tweet", views.api),
     path("", include("user.urls")),
+    path("", include("tweet.urls")),
 ]
