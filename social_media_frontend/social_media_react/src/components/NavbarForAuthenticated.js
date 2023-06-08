f31e5ccc4a3e6e7af0133d47982f0b6db76a464b
tanujsonare <tanujsonare1910@gmail.com>
2023-06-08 14:51:06 +0530

    updated nav bar search bar button

--- social_media_frontend/social_media_react/src/components/NavbarForAuthenticated.js
+++ social_media_frontend/social_media_react/src/components/NavbarForAuthenticated.js
@@ -5,7 +5,7 @@ export default function NavbarForAuthenticated(props) {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor:"rgb(1 4 6 / 46%)"}}>
-        <Link className="navbar-brand mx-2" to="/">TweeMe</Link>
+        <Link className="navbar-brand mx-3" to="/">TweeMe</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -26,9 +26,9 @@ export default function NavbarForAuthenticated(props) {
                     <Link className="nav-link" to="/logout">Logout</Link>
                 </li>
             </ul>
-            <form className="d-flex " role="search">
+            <form className="d-flex mx-3" role="search">
                 <input className="form-control me-2 mx-4" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success" type="submit">Search</button>
+                <button className="btn btn-outline-light" type="submit">Search</button>
             </form>
           </div>
       </nav>
