563b39d546553dbe7f6df430a9f43f448236d8f0
tanujsonare <tanujsonare1910@gmail.com>
2023-06-09 17:12:03 +0530

    Updated serializer to get is liked

--- social_media_backend/tweet/serializers.py
+++ social_media_backend/tweet/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
 from . import models
+from user.models import CustomUser
 
 
-class GetTweetSerializer(serializers.ModelSerializer):   
+class GetTweetSerializer(serializers.ModelSerializer):
+    is_liked = serializers.SerializerMethodField(read_only=True)
     
     class Meta:
         model = models.Tweet
@@ -13,7 +15,20 @@ class GetTweetSerializer(serializers.ModelSerializer):
             "content",
             "created_at",
             "likes",
+            "is_liked",
         ]
+
+    def get_is_liked(self, obj):
+        user_id = self.context.get("user_id") if self.context else None
+        if user_id:
+            user = CustomUser.objects.get(id=user_id)
+            is_liked = False
+            if user:
+                liked_tweet = models.Like.objects.filter(user=user, tweet=obj)
+                if liked_tweet:
+                    is_liked = True   
+                    return is_liked
+            return is_liked
 
 
 class AddTweetSerializer(serializers.ModelSerializer):
