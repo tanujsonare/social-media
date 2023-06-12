1f69ca4c4d4ebf4b423b43a0e6e31e2f804399ea
tanujsonare <tanujsonare1910@gmail.com>
2023-06-12 18:52:50 +0530

    Updated serializer file to get user name and is follwing flag

--- social_media_backend/tweet/serializers.py
+++ social_media_backend/tweet/serializers.py
@@ -6,6 +6,8 @@ from user.models import CustomUser
 
 class GetTweetSerializer(serializers.ModelSerializer):
     is_liked = serializers.SerializerMethodField(read_only=True)
+    user_name= serializers.SerializerMethodField(read_only=True)
+    is_following = serializers.SerializerMethodField(read_only=True)
     
     class Meta:
         model = models.Tweet
@@ -16,6 +18,8 @@ class GetTweetSerializer(serializers.ModelSerializer):
             "created_at",
             "likes",
             "is_liked",
+            "user_name",
+            "is_following"
         ]
 
     def get_is_liked(self, obj):
@@ -29,6 +33,24 @@ class GetTweetSerializer(serializers.ModelSerializer):
                     is_liked = True   
                     return is_liked
             return is_liked
+        
+    def get_user_name(self, obj):
+        user_name = obj.user.username
+        return user_name if user_name else None
+    
+    def get_is_following(self, obj):
+        user_id = self.context.get("user_id") if self.context else None
+        is_following = False
+        if user_id:
+            user = CustomUser.objects.get(id=user_id)
+            if user:
+                following_user = user.following.all()
+                if following_user:
+                    for f_user in following_user:
+                        if obj.user == f_user:
+                            is_following = True
+                            return is_following
+        return is_following
 
 
 class AddTweetSerializer(serializers.ModelSerializer):
