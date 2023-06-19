5bf41106b856c46c5dcff5172e0bc96ed25dc442
tanujsonare <tanujsonare1910@gmail.com>
2023-06-19 17:41:35 +0530

    Added website logo navbar

--- social_media_frontend/social_media_react/src/components/NavbarForAuthenticated.js
+++ social_media_frontend/social_media_react/src/components/NavbarForAuthenticated.js
@@ -1,11 +1,15 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
+import tweetLogo from './images/tweet_log.png'
+
 export default function NavbarForAuthenticated(props) {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor:"rgb(1 4 6 / 46%)"}}>
-        <Link className="navbar-brand mx-2" to="/">TweeMe</Link>
+        <Link className="navbar-brand mx-2" to="/">
+        <img src={tweetLogo} className="rounded-circle my-2 mx-2" width="40" height="40" role='button'/>
+          TweetMe</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
