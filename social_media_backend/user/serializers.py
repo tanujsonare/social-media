8deb3b897f5912e998bc36b8419cfd25058a411f
tanujsonare <tanujsonare1910@gmail.com>
2023-06-19 13:46:43 +0530

    updated get userprofile serializer

--- social_media_backend/user/serializers.py
+++ social_media_backend/user/serializers.py
@@ -16,15 +16,32 @@ class RegisterNewUserSerializer(serializers.ModelSerializer):
 
 class GetUserProfileSerializer(serializers.ModelSerializer):
     followers_count = serializers.SerializerMethodField(read_only=True)
+    is_following = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = CustomUser
         fields = [
+            "id",
             "username",
             "email",
             "bio",
-            "followers_count"
+            "followers_count",
+            "is_following",
         ]
 
     def get_followers_count(self, obj):
         followers_count = obj.followers.count()
         return followers_count if followers_count else 0
+    
+    def get_is_following(self, obj):
+        requested_user_id = self.context.get("requested_user_id") if self.context else None
+        is_following = False
+        if requested_user_id:
+            user = CustomUser.objects.get(id=requested_user_id)
+            if user:
+                following_user = user.following.all()
+                if following_user:
+                    for f_user in following_user:
+                        if obj == f_user:
+                            is_following = True
+                            return is_following
+        return is_following
