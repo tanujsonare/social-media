e977324b72468927e3d13a802461ad3b76299d90
tanujsonare <tanujsonare1910@gmail.com>
2023-06-20 13:45:28 +0530

    Added urls and settings for media

--- social_media_backend/social_media/settings.py
+++ social_media_backend/social_media/settings.py
@@ -145,6 +145,12 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
+# Base url to serve media files
+MEDIA_URL = '/media/'
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
