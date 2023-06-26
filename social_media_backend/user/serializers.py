bffe9bc7121a8c4027dbdc9f1a8889c0bf9116bf
tanujsonare <tanujsonare1910@gmail.com>
2023-06-26 13:07:39 +0530

    Updated serializer to get all details of user followers and following

--- social_media_backend/user/serializers.py
+++ social_media_backend/user/serializers.py
@@ -18,6 +18,8 @@ class GetUserProfileSerializer(serializers.ModelSerializer):
     followers_count = serializers.SerializerMethodField(read_only=True)
     following_count = serializers.SerializerMethodField(read_only=True)
     is_following = serializers.SerializerMethodField(read_only=True)
+    following_user = serializers.SerializerMethodField(read_only=True)
+    followers_user = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = CustomUser
         fields = [
@@ -29,6 +31,8 @@ class GetUserProfileSerializer(serializers.ModelSerializer):
             "followers_count",
             "is_following",
             "following_count",
+            "following_user",
+            "followers_user",
         ]
 
     def get_followers_count(self, obj):
@@ -52,3 +56,35 @@ class GetUserProfileSerializer(serializers.ModelSerializer):
                             is_following = True
                             return is_following
         return is_following
+    
+    def get_following_user(self, obj):
+        requested_user_id = self.context.get("requested_user_id") if self.context else None
+        following_user = obj.following.all()
+        following_list = []
+        for user in following_user:
+            following_user_dict = {}
+            following_user_dict["id"] = user.id
+            following_user_dict["username"] = user.username
+            following_user_dict["profile_image"] = user.profile_image.url if user.profile_image else None
+            try:
+                following_user_dict["is_following"] = CustomUser.objects.filter(id=requested_user_id, following = user).exists()
+            except:
+                following_user_dict["is_following"] = False
+            following_list.append(following_user_dict)
+        return following_list if following_list else None
+    
+    def get_followers_user(self, obj):
+        requested_user_id = self.context.get("requested_user_id") if self.context else None
+        followers_user = obj.followers.all()
+        followers_list = []
+        for user in followers_user:
+            followers_user_dict = {}
+            followers_user_dict["id"] = user.id
+            followers_user_dict["username"] = user.username
+            followers_user_dict["profile_image"] = user.profile_image.url if user.profile_image else None
+            try:
+                followers_user_dict["is_following"] = CustomUser.objects.filter(id=requested_user_id, following = user).exists()
+            except:
+                followers_user_dict["is_following"] = False
+            followers_list.append(followers_user_dict)
+        return followers_list if followers_list else None
