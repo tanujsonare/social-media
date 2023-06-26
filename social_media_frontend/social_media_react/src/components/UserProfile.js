20372ca092176146ad383468388ba46754691a1d
tanujsonare <tanujsonare1910@gmail.com>
2023-06-26 13:08:01 +0530

    Updated user following list details

--- social_media_frontend/social_media_react/src/components/UserProfile.js
+++ social_media_frontend/social_media_react/src/components/UserProfile.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import backgroundImage from './images/social_media_back.jpg'
 import tweetBackgroundImage from './images/tweet_image.jpg'
@@ -6,6 +7,7 @@ import NavbarForAuthenticated from './NavbarForAuthenticated'
 import defaultProfileImage from './images/default_profile_img.webp'
 
 export default function UserProfile(props) {
+    const navigate = useNavigate();
     const [userData, setUserData] = useState(null);
     var userId;
     if (window.location.href.includes("user_id=")) {
@@ -16,6 +18,8 @@ export default function UserProfile(props) {
     // to call get_user_profile api 
     useEffect(() => {
         props.getUserProfile(userId);
+        document.getElementById('followingModal').style.display = 'none';
+        document.getElementById('followersModal').style.display = 'none';
     }, [userId]);
 
     // to get user profile data
@@ -30,50 +34,91 @@ export default function UserProfile(props) {
         props.getUserProfile(userId);
     }
 
-    const modalShow = () => {
-        document.getElementById('followingModal').style.display = 'block'
+    const followingModalShow = () => {
+        document.getElementById('followingModal').style.display = 'block';
+        document.getElementsByClassName('user_profile')[0].style.display = 'none';
     }
 
-    const hideModal = () => {
-        document.getElementById('followingModal').style.display = 'none'
+    const hideFollowingModal = () => {
+        document.getElementById('followingModal').style.display = 'none';
+        document.getElementsByClassName('user_profile')[0].style.display = 'block';
+    }
+    
+    const followersModalShow = () => {
+        document.getElementById('followersModal').style.display = 'block';
+        document.getElementsByClassName('user_profile')[0].style.display = 'none';
+    }
+
+    const hideFollowersModal = () => {
+        document.getElementById('followersModal').style.display = 'none';
+        document.getElementsByClassName('user_profile')[0].style.display = 'block';
+    }
+
+    const showUserProfile = (e) =>{
+        const userId = e.target.getAttribute("userid");
+        hideFollowingModal();
+        navigate(`/profile?user_id=${userId}`);
     }
 
     return (
         <div style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', width: '100%', minHeight: '700px' }}>
             <NavbarForAuthenticated userName={props.userName} userToken={props.userToken} />
 
-            <div className="w3-container">
-                {/* <button  className="w3-button w3-black">Open Modal</button> */}
-
-                <div id="followingModal" className="w3-modal">
-                    <div className="w3-modal-content">
-                        <div class="container mt-5">
-                            <div class="d-flex justify-content-center row">
-                                <div class="col-md-6">
-                                    <div class="p-3 bg-white text-center">
-                                    <header className="w3-container w3-teal">
-                                        <span onClick={hideModal} className="w3-button w3-display-topright" role='button'>&times;</span>
-                                        <h2>Users you follow</h2>
+            {/* following modal  */}
+            <div id="followingModal" className="w3-modal">
+                <div className="s3-modal-content">
+                    <div className="container mt-5">
+                        <div className="d-flex justify-content-center row">
+                            <div className="col-md-6 card">
+                                <div className="p-3 bg-white text-center">
+                                    <header className="card-header d-flex justify-content-between align-items-center">
+                                        <h3 className="text-center flex-grow-1">User followed by {userData && userData.username}</h3>
+                                        <span onClick={hideFollowingModal} className="text-end mb-5" role="button" style={{ fontSize: "30px" }}>
+                                            &times;
+                                        </span>
                                     </header>
-                                        <div class="d-flex flex-row justify-content-between align-items-center">
-                                            <div class="d-flex flex-row align-items-center"><img class="rounded-circle" src="https://i.imgur.com/KmT515u.jpg" width="55" />
-                                                <div class="d-flex flex-column align-items-start ml-2"><span class="font-weight-bold">Bella Thorne</span><span class="followers">1450 Followers</span></div>
+                                    {userData && userData.following_user && userData.following_user.map((element) => {
+                                    return <div className="d-flex flex-row justify-content-between align-items-center my-3">
+                                            <div className="d-flex flex-row align-items-center"><img className="rounded-circle" src={element.profile_image ? element.profile_image.includes("media/") ? element.profile_image : "/media/" + element.profile_image : defaultProfileImage} width="30" height="30"  onClick={showUserProfile} userid={element.id} role='button'/>
+                                                <div className="d-flex flex-column align-items-start ml-2"><span className="font-weight-bold mx-3">{element.username}</span></div>
                                             </div>
-                                            <div class="d-flex flex-row align-items-center mt-2"><button class="btn btn-outline-primary btn-sm" type="button">Follow</button></div>
-                                        </div>
-                                        <div class="d-flex flex-row justify-content-between align-items-center mt-2">
-                                            <div class="d-flex flex-row align-items-center"><img class="rounded-circle" src="https://i.imgur.com/I6WnkOv.jpg" width="55" />
-                                                <div class="d-flex flex-column align-items-start ml-2"><span class="font-weight-bold">Scarlet</span><span class="followers">18570 Followers</span></div>
+                                            <div className="d-flex flex-row align-items-center mt-2">
+                                                {userData.username == props.userName && element.is_following && <button className="btn btn-dark btn-sm" type="button">Unfollow</button>}
+                                                {userData.username !== props.userName && element.is_following && <button className="btn btn-dark btn-sm" type="button" disabled>Following</button>}
+                                                {element.username !== props.userName && !element.is_following && <button className="btn btn-dark btn-sm" type="button">Follow</button>}
                                             </div>
-                                            <div class="d-flex flex-row align-items-center mt-2"><button class="btn btn-primary btn-sm" type="button">Following</button></div>
                                         </div>
-                                        <div class="d-flex flex-row justify-content-between align-items-center mt-2">
-                                            <div class="d-flex flex-row align-items-center"><img class="rounded-circle" src="https://i.imgur.com/eEo8asG.jpg" width="55" />
-                                                <div class="d-flex flex-column align-items-start ml-2"><span class="font-weight-bold">Soffie Morne</span><span class="followers">12550 Followers</span></div>
+                                    })
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            {/* followers modal  */}
+            <div id="followersModal" className="w3-modal">
+                <div className="w3-modal-content">
+                    <div className="container mt-5">
+                        <div className="d-flex justify-content-center row">
+                            <div className="col-md-6 card">
+                                <div className="p-3 bg-white text-center">
+                                    <header className="card-header d-flex justify-content-between align-items-center">
+                                        <h3 className="text-center flex-grow-1">Followers of {userData && userData.username}</h3>
+                                        <span onClick={hideFollowersModal} className="text-end mb-5" role="button" style={{ fontSize: "30px" }}>
+                                            &times;
+                                        </span>
+                                    </header>
+                                    {userData && userData.followers_user && userData.followers_user.map((element) => {
+                                    return <div className="d-flex flex-row justify-content-between align-items-center my-3">
+                                            <div className="d-flex flex-row align-items-center"><img className="rounded-circle" src={element.profile_image ? element.profile_image.includes("media/") ? element.profile_image : "/media/" + element.profile_image : defaultProfileImage} width="30" height="30"  onClick={showUserProfile} userid={element.id} role='button'/>
+                                                <div className="d-flex flex-column align-items-start ml-2"><span className="font-weight-bold mx-3">{element.username}</span></div>
                                             </div>
-                                            <div class="d-flex flex-row align-items-center mt-2"><button class="btn btn-outline-primary btn-sm" type="button">Follow</button></div>
+                                            <div className="d-flex flex-row align-items-center mt-2"><button className="btn btn-dark btn-sm" type="button">Follow</button></div>
                                         </div>
-                                    </div>
+                                    })
+                                    }
                                 </div>
                             </div>
                         </div>
@@ -81,7 +126,7 @@ export default function UserProfile(props) {
                 </div>
             </div>
 
-            <div className='my-5'>
+            <div className='my-5 user_profile'>
                 <div className="container d-flex justify-content-center align-items-center my-4">
                     <div className="card" style={{ minHeight: "380px", minWidth: "400px" }}>
                         <div style={{ backgroundImage: `url(${tweetBackgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', width: '100%', height: '220px' }}>
@@ -105,11 +150,11 @@ export default function UserProfile(props) {
                                 </div>
                                 <div className="stats mx-4">
                                     <h6 className="mb-0">Following</h6>
-                                    <span onClick={modalShow} role='button'>{userData && userData.following_count}</span>
+                                    <span onClick={followingModalShow} role='button'>{userData && userData.following_count}</span>
                                 </div>
                                 <div className="stats mx-4">
                                     <h6 className="mb-0">Followers</h6>
-                                    <span>{userData && userData.followers_count}</span>
+                                    <span onClick={followersModalShow} role='button'>{userData && userData.followers_count}</span>
                                 </div>
                             </div>
                         </div>
