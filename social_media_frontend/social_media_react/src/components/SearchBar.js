6327c5f0b8797188431e1fe29b5850f6166c159d
tanujsonare <tanujsonare1910@gmail.com>
2023-07-03 16:36:25 +0530

    Fixed follow button issue and updated search box card

--- social_media_frontend/social_media_react/src/components/SearchBar.js
+++ social_media_frontend/social_media_react/src/components/SearchBar.js
@@ -31,35 +31,33 @@ export default function SearchBar(props) {
 
     return (
         <div style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', width: '100%', minHeight: '700px' }}>
-            <NavbarForAuthenticated userName={props.userName} userToken={props.userToken}/>
+            <NavbarForAuthenticated userName={props.userName} userToken={props.userToken} />
             <div className="row my-2">
                 {usersData && usersData.map((element) => {
                     return <div className="col-md-11 mx-auto d-flex justify-content-center" key={element.id}>
-                        <div className="card my-2" style={{ minWidth: "700px", maxWidth:"700px" }}>
-                            <div className='d-flex justify-content-start'>
-                                <img className="card-i-mg-top" src={element.profile_image ? element.profile_image : defaultProfileImage} style={{ width: "30%", minHeight:"100px", maxHeight: "200px", backgroundSize: 'cover' }} />
-                                <div className="row mx-5 me-0">
-                                    <div className="col-sm-9">
-                                        <h2 className="my-4">{element.username}</h2>
-                                        <p className="mx-2 w-100 text-secondary" style={{ textAlign: "center" }}>{element.bio}</p>
-                                        <div className="d-flex justify-content-start">
-                                            <div className="mx-4">
-                                                <h6 className="my-2">Followers</h6>
-                                                <span >{element.followers_count}</span>
-                                            </div>
-                                            <div className="mx-4">
-                                                <h6 className="my-2">Following</h6>
-                                                <span >{element.following_count}</span>
-                                            </div>
-                                            <div className="mx-4">
-                                                <h6 className="my-2">Posts</h6>
-                                                <span>WIP</span>
-                                            </div>
+                        <div className="card my-2" style={{ minWidth: "700px", maxWidth: "700px" }}>
+                            <div className="card-body d-flex">
+                                <img className="card-i-mg-top" src={element.profile_image ? element.profile_image : defaultProfileImage} style={{ width: "30%", minHeight: "100px", maxHeight: "200px", backgroundSize: 'cover' }} />
+                                <div className="flex-grow-1 mx-3">
+                                    <h2 className="my-4">{element.username}</h2>
+                                    <p className="w-100 text-secondary" style={{ textAlign: "center" }}>{element.bio}</p>
+                                    <div className="d-flex justify-content-start">
+                                        <div className="mx-4">
+                                            <h6 className="my-2">Followers</h6>
+                                            <span>{element.followers_count}</span>
+                                        </div>
+                                        <div className="mx-4">
+                                            <h6 className="my-2">Following</h6>
+                                            <span>{element.following_count}</span>
+                                        </div>
+                                        <div className="mx-4">
+                                            <h6 className="my-2">Posts</h6>
+                                            <span>WIP</span>
                                         </div>
                                     </div>
                                 </div>
-                                <div className='d-flex justify-content-end'>
-                                    <button className='btn btn-dark btn-sm my-5 rounded-5' style={{marginBottom:"164px !important"}}> Follow <i className="fa-solid fa-circle-plus" style={{color: "#fff",}} ></i></button>
+                                <div className="ml-auto align-self-start mx-4">
+                                    <button className="btn btn-dark btn-sm rounded-5 d-flex align-items-center">Follow <i className="fa-solid fa-circle-plus d-flex align-items-center mx-1" style={{ color: "#fff" }}></i></button>
                                 </div>
                             </div>
                         </div>
