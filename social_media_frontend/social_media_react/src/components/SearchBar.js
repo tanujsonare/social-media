a54cb42542769783dca0710df1daf57813bc4bab
tanujsonare <tanujsonare1910@gmail.com>
2023-07-03 12:10:36 +0530

    Show users data on search page

--- social_media_frontend/social_media_react/src/components/SearchBar.js
+++ social_media_frontend/social_media_react/src/components/SearchBar.js
@@ -1,42 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 import backgroundImage from './images/social_media_back.jpg'
 import NavbarForAuthenticated from './NavbarForAuthenticated'
+import defaultProfileImage from './images/default_profile_img.webp'
 
 export default function SearchBar(props) {
+    var searchText;
+    if (window.location.href.includes("user_name=")) {
+        let text = window.location.href.split("user_name=");
+        if (text) {
+            searchText = text[1];
+        }
+    }
+    const [usersData, setUsersData] = useState([]);
+
+    useEffect(() => {
+        const getSearchUsers = async () => {
+            await axios.get(`http://127.0.0.1/api/search_user?requested_user_id=${props.userId}&user_name=${searchText}`
+            ).then(response => {
+                setUsersData(response.data.user_profiles)
+            })
+                .catch(error => {
+                    console.log(error.response.data);
+                });
+        }
+        getSearchUsers();
+    }, searchText)
+
     return (
         <div style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', width: '100%', minHeight: '700px' }}>
             <NavbarForAuthenticated userName={props.userName} userToken={props.userToken} />
-            <div className="container">
-                <div className="row ng-scope">
-                    <div className="col-md-11 mx-auto my-5 d-flex justify-content-center">
-                        <div className="card my-3" style={{maxWidth: "800px"}}>
+            <div className="row my-5">
+                {usersData && usersData.map((element) => {
+                    return <div className="col-md-11 mx-auto d-flex justify-content-center">
+                        <div className="card my-2" style={{ maxWidth: "1000px" }}>
                             <div className='d-flex justify-content-start'>
-                                <img className="card-i-mg-top" src="https://bootdey.com/img/Content/avatar/avatar1.png" style={{ width: "30%", maxHeight: "250px", backgroundSize: 'cover' }} />
+                                <img className="card-i-mg-top" src={element.profile_image ? element.profile_image : defaultProfileImage} style={{ width: "30%", maxHeight: "250px", backgroundSize: 'cover' }} />
                                 <div className="row mx-5 me-0">
                                     <div className="col-sm-9">
-                                        <h2 className="my-4">Tanuj sonare</h2>
-                                        <p className="mx-2 w-100" style={{textAlign: "justify"}}>Not just usual Metro. But something bigger. Not just usual widgets, but real widgets. Not just usual Metro. But something bigger. Not just usual widgets, but real widgets.</p>
+                                        <h2 className="my-4">{element.username}</h2>
+                                        <p className="mx-2 w-100 text-secondary" style={{ textAlign: "center" }}>{element.bio}</p>
                                         <div className="d-flex justify-content-start">
                                             <div className="mx-4">
                                                 <h6 className="my-2">Followers</h6>
-                                                <span >100</span>
+                                                <span >{element.followers_count}</span>
                                             </div>
                                             <div className="mx-4">
                                                 <h6 className="my-2">Following</h6>
-                                                <span >100</span>
+                                                <span >{element.following_count}</span>
                                             </div>
                                             <div className="mx-4">
                                                 <h6 className="my-2">Posts</h6>
-                                                <span> 100 </span>
+                                                <span>WIP</span>
                                             </div>
-                                        </div> 
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                })
+                }
             </div>
         </div>
     )
