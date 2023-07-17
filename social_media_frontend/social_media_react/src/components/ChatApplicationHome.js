99d3e4127c851d92f0edb7fba41a9068f8a3dc52
tanujsonare <tanujsonare1910@gmail.com>
2023-07-17 16:35:04 +0530

    Updated chat page UI

--- social_media_frontend/social_media_react/src/components/ChatApplicationHome.js
+++ social_media_frontend/social_media_react/src/components/ChatApplicationHome.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
 import NavbarForAuthenticated from './NavbarForAuthenticated'
-import backgroundImage from './images/social_media_back.jpg'
 
 export default function ChatApplicationHome(props) {
     return (
         <div className="custom-background" style={{ backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', width: '100%', minHeight: '700px' }}>
             <NavbarForAuthenticated userName={props.userName} userToken={props.userToken} />
-            <section className="">
+            <section >
                 <div className="container py-5">
 
                     <div className="row">
@@ -33,97 +32,6 @@ export default function ChatApplicationHome(props) {
                                                 <div className="pt-1">
                                                     <p className="small text-white mb-1">Just now</p>
                                                     <span className="badge bg-danger float-end">1</span>
-                                                </div>
-                                            </a>
-                                        </li>
-                                        <li className="p-2 border-bottom" style={{ borderBottom: "1px solid rgba(255,255,255,.3) !important;" }}>
-                                            <a href="#!" className="d-flex justify-content-between link-light">
-                                                <div className="d-flex flex-row">
-                                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-1.webp" alt="avatar"
-                                                        className="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60" />
-                                                        <div className="pt-1">
-                                                            <p className="fw-bold mb-0">Danny Smith</p>
-                                                            <p className="small text-white">Lorem ipsum dolor sit.</p>
-                                                        </div>
-                                                </div>
-                                                <div className="pt-1">
-                                                    <p className="small text-white mb-1">5 mins ago</p>
-                                                </div>
-                                            </a>
-                                        </li>
-                                        <li className="p-2 border-bottom" style={{ borderBottom: "1px solid rgba(255,255,255,.3) !important;" }}>
-                                            <a href="#!" className="d-flex justify-content-between link-light">
-                                                <div className="d-flex flex-row">
-                                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-2.webp" alt="avatar"
-                                                        className="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60" />
-                                                        <div className="pt-1">
-                                                            <p className="fw-bold mb-0">Alex Steward</p>
-                                                            <p className="small text-white">Lorem ipsum dolor sit.</p>
-                                                        </div>
-                                                </div>
-                                                <div className="pt-1">
-                                                    <p className="small text-white mb-1">Yesterday</p>
-                                                </div>
-                                            </a>
-                                        </li>
-                                        <li className="p-2 border-bottom" style={{ borderBottom: "1px solid rgba(255,255,255,.3) !important;" }}>
-                                            <a href="#!" className="d-flex justify-content-between link-light">
-                                                <div className="d-flex flex-row">
-                                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-3.webp" alt="avatar"
-                                                        className="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60" />
-                                                        <div className="pt-1">
-                                                            <p className="fw-bold mb-0">Ashley Olsen</p>
-                                                            <p className="small text-white">Lorem ipsum dolor sit.</p>
-                                                        </div>
-                                                </div>
-                                                <div className="pt-1">
-                                                    <p className="small text-white mb-1">Yesterday</p>
-                                                </div>
-                                            </a>
-                                        </li>
-                                        <li className="p-2 border-bottom" style={{ borderBottom: "1px solid rgba(255,255,255,.3) !important;" }}>
-                                            <a href="#!" className="d-flex justify-content-between link-light">
-                                                <div className="d-flex flex-row">
-                                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-4.webp" alt="avatar"
-                                                        className="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60" />
-                                                        <div className="pt-1">
-                                                            <p className="fw-bold mb-0">Kate Moss</p>
-                                                            <p className="small text-white">Lorem ipsum dolor sit.</p>
-                                                        </div>
-                                                </div>
-                                                <div className="pt-1">
-                                                    <p className="small text-white mb-1">Yesterday</p>
-                                                </div>
-                                            </a>
-                                        </li>
-                                        <li className="p-2 border-bottom" style={{ borderBottom: "1px solid rgba(255,255,255,.3) !important;" }}>
-                                            <a href="#!" className="d-flex justify-content-between link-light">
-                                                <div className="d-flex flex-row">
-                                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp" alt="avatar"
-                                                        className="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60" />
-                                                        <div className="pt-1">
-                                                            <p className="fw-bold mb-0">Lara Croft</p>
-                                                            <p className="small text-white">Lorem ipsum dolor sit.</p>
-                                                        </div>
-                                                </div>
-                                                <div className="pt-1">
-                                                    <p className="small text-white mb-1">Yesterday</p>
-                                                </div>
-                                            </a>
-                                        </li>
-                                        <li className="p-2">
-                                            <a href="#!" className="d-flex justify-content-between link-light">
-                                                <div className="d-flex flex-row">
-                                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp" alt="avatar"
-                                                        className="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60" />
-                                                        <div className="pt-1">
-                                                            <p className="fw-bold mb-0">Brad Pitt</p>
-                                                            <p className="small text-white">Lorem ipsum dolor sit.</p>
-                                                        </div>
-                                                </div>
-                                                <div className="pt-1">
-                                                    <p className="small text-white mb-1">5 mins ago</p>
-                                                    <span className="text-white float-end"><i className="fas fa-check" aria-hidden="true"></i></span>
                                                 </div>
                                             </a>
                                         </li>
@@ -170,23 +78,6 @@ export default function ChatApplicationHome(props) {
                                     </div>
                                     <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp" alt="avatar"
                                         className="rounded-circle d-flex align-self-start ms-3 shadow-1-strong" width="60" />
-                                </li>
-                                <li className="d-flex justify-content-between mb-4">
-                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp" alt="avatar"
-                                        className="rounded-circle d-flex align-self-start me-3 shadow-1-strong" width="60" />
-                                        <div className="card mask-custom">
-                                            <div className="card-header d-flex justify-content-between p-3"
-                                                style={{ borderBottom: "1px solid rgba(255,255,255,.3);" }}>
-                                                <p className="fw-bold mb-0">Brad Pitt</p>
-                                                <p className="text-light small mb-0"><i className="far fa-clock"></i> 10 mins ago</p>
-                                            </div>
-                                            <div className="card-body">
-                                                <p className="mb-0">
-                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                                                    labore et dolore magna aliqua.
-                                                </p>
-                                            </div>
-                                        </div>
                                 </li>
                                 <li className="mb-3">
                                     <div className="form-outline form-white">
