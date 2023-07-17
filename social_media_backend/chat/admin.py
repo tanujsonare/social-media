31c7cd775fca89544388a365782832a337f65118
tanujsonare <tanujsonare1910@gmail.com>
2023-07-17 18:21:11 +0530

    Added a new model message in chat app

--- social_media_backend/chat/admin.py
+++ social_media_backend/chat/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Message
+
+
+@admin.register(Message)
+class TweetAdmin(admin.ModelAdmin):
+    list_display=[
+        'sender_user',
+        'receiver_user',
+        'created_at',
+    ]
--- social_media_backend/chat/models.py
+++ social_media_backend/chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from user.models import CustomUser
+
+
+class Message(models.Model):
+    sender_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="message_send_by")
+    receiver_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="message_received_to")
+    content = models.TextField()
+    media = models.FileField(upload_to="images/", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
