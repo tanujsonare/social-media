461ff61295d18d7cc5f3b2f77a40688384d13101
tanujsonare <tanujsonare1910@gmail.com>
2023-07-20 11:26:57 +0530

    Added scroller on chat application

--- social_media_frontend/social_media_react/src/components/ChatApplicationHome.js
+++ social_media_frontend/social_media_react/src/components/ChatApplicationHome.js
@@ -25,7 +25,6 @@ export default function ChatApplicationHome(props) {
     useEffect(() => {
         searchUsersForChat();
     })
-    
 
     return (
         <div className="custom-background" style={{ backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', width: '100%', minHeight: '700px' }}>
@@ -40,7 +39,7 @@ export default function ChatApplicationHome(props) {
                             </Link>
                             <div className="card mask-custom my-4">
                                 <div className="card-body">
-                                    <ul className="list-unstyled mb-0" style={{minHeight:"400px"}}>
+                                    <ul className="list-unstyled mb-0 overflow-auto" style={{minHeight:"400px", maxHeight:"470px"}}>
                                         {searchData && searchData.map((element) => {
                                             return element.is_following == true && element.is_conversation && <li className="p-2 border-bottom search_user" style={{ borderBottom: "1px solid rgba(255,255,255,.3) !important" }} key={element.id}>
                                                 <div className="d-flex justify-content-between link-light">
@@ -69,9 +68,9 @@ export default function ChatApplicationHome(props) {
 
                         </div>
 
-                        {/* <div className="col-md-6 col-lg-7 col-xl-7">
+                        <div className="col-md-6 col-lg-7 col-xl-7">
 
-                            <ul className="list-unstyled text-white">
+                            <ul className="list-unstyled text-white overflow-auto" style={{ maxHeight: "380px", minHeight:"380px"}}>
                                 <li className="d-flex justify-content-between mb-4">
                                     <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp" alt="avatar"
                                         className="rounded-circle d-flex align-self-start me-3 shadow-1-strong" width="60" />
@@ -106,16 +105,82 @@ export default function ChatApplicationHome(props) {
                                     <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp" alt="avatar"
                                         className="rounded-circle d-flex align-self-start ms-3 shadow-1-strong" width="60" />
                                 </li>
-                                <li className="mb-3">
-                                    <div className="form-outline form-white">
-                                        <textarea className="form-control" id="textAreaExample3" rows="4"></textarea>
-                                        <label className="form-label" htmlFor="textAreaExample3">Message</label>
+                                <li className="d-flex justify-content-between mb-4">
+                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp" alt="avatar"
+                                        className="rounded-circle d-flex align-self-start me-3 shadow-1-strong" width="60" />
+                                    <div className="card mask-custom">
+                                        <div className="card-header d-flex justify-content-between p-3"
+                                            style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}>
+                                            <p className="fw-bold mb-0">Brad Pitt</p>
+                                            <p className="text-light small mb-0"><i className="far fa-clock"></i> 12 mins ago</p>
+                                        </div>
+                                        <div className="card-body">
+                                            <p className="mb-0">
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                                labore et dolore magna aliqua.
+                                            </p>
+                                        </div>
                                     </div>
                                 </li>
-                                <button type="button" className="btn btn-light btn-lg btn-rounded float-end">Send</button>
+                                <li className="d-flex justify-content-between mb-4">
+                                    <div className="card mask-custom w-100">
+                                        <div className="card-header d-flex justify-content-between p-3"
+                                            style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}>
+                                            <p className="fw-bold mb-0">Lara Croft</p>
+                                            <p className="text-light small mb-0"><i className="far fa-clock"></i> 13 mins ago</p>
+                                        </div>
+                                        <div className="card-body">
+                                            <p className="mb-0">
+                                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                                                laudantium.
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp" alt="avatar"
+                                        className="rounded-circle d-flex align-self-start ms-3 shadow-1-strong" width="60" />
+                                </li>
+                                <li className="d-flex justify-content-between mb-4">
+                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp" alt="avatar"
+                                        className="rounded-circle d-flex align-self-start me-3 shadow-1-strong" width="60" />
+                                    <div className="card mask-custom">
+                                        <div className="card-header d-flex justify-content-between p-3"
+                                            style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}>
+                                            <p className="fw-bold mb-0">Brad Pitt</p>
+                                            <p className="text-light small mb-0"><i className="far fa-clock"></i> 12 mins ago</p>
+                                        </div>
+                                        <div className="card-body">
+                                            <p className="mb-0">
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                                labore et dolore magna aliqua.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li className="d-flex justify-content-between mb-4">
+                                    <div className="card mask-custom w-100">
+                                        <div className="card-header d-flex justify-content-between p-3"
+                                            style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}>
+                                            <p className="fw-bold mb-0">Lara Croft</p>
+                                            <p className="text-light small mb-0"><i className="far fa-clock"></i> 13 mins ago</p>
+                                        </div>
+                                        <div className="card-body">
+                                            <p className="mb-0">
+                                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                                                laudantium.
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp" alt="avatar"
+                                        className="rounded-circle d-flex align-self-start ms-3 shadow-1-strong" width="60" />
+                                </li>
                             </ul>
+                            <div className="form-outline form-white">
+                                <textarea className="form-control" id="textAreaExample3" rows="4"></textarea>
+                                <label className="form-label" htmlFor="textAreaExample3">Message</label>
+                            </div>
+                            <button type="button" className="btn btn-light btn-lg btn-rounded float-end">Send</button>
 
-                        </div> */}
+                        </div>
 
                     </div>
 
