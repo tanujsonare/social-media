303128b2c450bd92d9acaa9aac0f503cfe6f8997
tanujsonare <tanujsonare1910@gmail.com>
2023-07-31 16:07:44 +0530

    Updated asgi file to chat urls

--- social_media_backend/social_media/asgi.py
+++ social_media_backend/social_media/asgi.py
@@ -10,7 +10,21 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from chat.consumers import ChatRoomConsumer
+from django.urls import path
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "social_media.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter([
+            path('ws/chat/', ChatRoomConsumer.as_asgi()),
+            path('ws/chat/<str:room_name>', ChatRoomConsumer.as_asgi()),
+        ])
+    ),
+})
