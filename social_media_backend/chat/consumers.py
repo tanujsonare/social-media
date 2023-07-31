98acc10e0dc01383b24f39541dadc723b8246abb
tanujsonare <tanujsonare1910@gmail.com>
2023-07-31 16:06:41 +0530

    Added consumers for web sockets

---
+++ social_media_backend/chat/consumers.py
@@ -0,0 +1,53 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+
+class ChatRoomConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+        # await self.channel_layer.group_send(
+        #     self.room_group_name,
+        #     {
+        #         'type': 'tester_message',
+        #         'tester': 'Hello, testuser',
+        #     }
+        # )
+
+    # async def tester_message(self, event):
+    #     tester = event['tester']
+
+    #     await self.send(text_data=json.dumps({
+    #         'tester': tester,
+    #     }))
+        
+    
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        message_text_json = json.loads(text_data)
+        message = message_text_json["message"]
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": message,
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event["message"]
+        await self.send(text_data=json.dumps(
+            {"message": message}
+        ))
+        
